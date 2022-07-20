34573634708d55200f3afd1eb96773eeeb2c1831
Milad <miladsoft@yahoo.com>
2022-07-20 10:43:08 +0430

    change update service

--- angular/src/app/services/app-update.service.ts
+++ angular/src/app/services/app-update.service.ts
@@ -1,15 +1,40 @@
-import { Injectable } from '@angular/core';
+import { ApplicationRef, Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SwUpdate } from '@angular/service-worker';
+import { interval } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AppUpdateService {
-  constructor(private readonly updates: SwUpdate, private snackBar: MatSnackBar) {
-    this.updates.available.subscribe(event => {
+  constructor(private readonly update: SwUpdate, private snackBar: MatSnackBar, private appRef: ApplicationRef) {
+    this.updateClient();
+    this.checkUpdate();
+  }
+
+  updateClient() {
+    if (!this.update.isEnabled) {
+      console.log('Not Enabled');
+      return;
+    }
+    this.update.available.subscribe((event) => {
+      console.log(`current`, event.current, `available `, event.available);
       this.showAppUpdateAlert();
     });
   }
+
+  checkUpdate() {
+    this.appRef.isStable.subscribe((isStable) => {
+      if (isStable) {
+        const timeInterval = interval(8 * 60 * 60 * 1000);
+
+        timeInterval.subscribe(() => {
+          this.update.checkForUpdate().then(() => console.log('checked'));
+          console.log('update checked');
+        });
+      }
+    });
+  }
+
   showAppUpdateAlert() {
     let sb = this.snackBar.open('App Update available!', 'Update', {
       horizontalPosition: 'center',
@@ -20,6 +45,6 @@ export class AppUpdateService {
     });
   }
   doAppUpdate() {
-    this.updates.activateUpdate().then(() => document.location.reload());
+    this.update.activateUpdate().then(() => document.location.reload());
   }
 }
